f1bd7d7bee6c10b55e36f2b8328b044e78dd0a14
Alexandre Pénombre <alexandre.penombre@gmail.com>
2019-07-06 23:34:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express'); 
+const bodyParser = require('body-parser')
+const lcdi2c = require('lcdi2c');
+const path = require('path');
+
+// Define our LCD Screen (get the address with : i2cdetect -y 1)
+const lcd = new lcdi2c(1, 0x27, 16, 2);
+
+const app = express();
+
+function SetLCD(line1, line2) {
+  lcd.clear();
+  lcd.println(line1, 1);
+  lcd.println(line2, 2);
+}
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
+app.post('/', (req, res) => {
+  const data = req.body;
+  
+  if(data.line1 || data.line2) {
+    SetLCD(date.line1, data.line2);
+  }
+
+  return res.redirect('/?date' + Date.now() + '&result=1');
+})
+
+app.listen('3050', () => {
+  console.log('listening on 3050');
+})
